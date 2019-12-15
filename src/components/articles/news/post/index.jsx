c7917670981fd204a51891d5698cd46cd30d3ff2
Enoch Ezeani <40115382+eizodann@users.noreply.github.com>
2019-12-15 22:54:06 +0100

    first article post view commit

---
+++ src/components/articles/news/post/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Axios from "axios";
+import { URL } from "../../../../config";
+import style from "../../articles.module.css";
+
+
+
+class NewsArticles extends Component {
+    render() {
+        return (
+            <div>
+                News
+            </div>
+        );
+    }
+}
+
+export default NewsArticles;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/home/home.jsx";
 import Layout from "./hoc/layout/layout.jsx";
+import NewsArticle from "./components/articles/news/post/index.jsx";
 
 class Routes extends Component {
   render() {
     return (
       <Layout>
         <Switch>
-          <Route path="/" exact component={Home}></Route>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/articles/:id" exact component={NewsArticle}></Route>
         </Switch>
       </Layout>
     );

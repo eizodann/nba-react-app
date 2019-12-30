a772232f80f561b06f7df24e662d56f9a6f2aac6
Enoch Ezeani <40115382+eizodann@users.noreply.github.com>
2019-12-30 18:10:14 +0100

    dashboard completed

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import VideoArticle from "./components/articles/videos/video/index.jsx";
 import NewsMain from "./components/articles/news/main/index.jsx";
 import VideosMain from "./components/articles/videos/main/index.jsx";
 import SignIn from "./components/signin/signin.jsx";
+import Dashboard from "./components/dashboard/dashboard.jsx";
 
 const Routes =(props)=> {
     console.log('this.props :', props);
@@ -19,6 +20,8 @@ const Routes =(props)=> {
           <Route path="/videos/:id" exact component={VideoArticle}></Route>
           <Route path="/videos" exact component={VideosMain}></Route>
           <Route path="/sign-in" exact component={SignIn}></Route>
+          <Route path="/dashboard" exact component={Dashboard}></Route>
+
         </Switch>
       </Layout>
     );
